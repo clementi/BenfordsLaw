ffeec0004e862c6e95d36b1f674022950f73d3a8
Jeffrey Pratt <jeff@metronomix.com>
2013-03-04 21:08:17 -0700

    Added get_first_digit function

--- main.R
+++ main.R
@@ -1,3 +1,11 @@
 library(MASS)
 
-truehist(sapply(gdp_2001_numbers_only$V1, bben), main="Distribution of the First Digit of 229 GDPs", ylab="Frequency", xlab="First digit")
+get_first_digit <- function(k) {
+  as.numeric(head(strsplit(as.character(k), '')[[1]], n=1))
+}
+
+truehist(
+  sapply(gdp_2001_numbers_only$V1, get_first_digit),
+  main="Distribution of the First Digit of 229 GDPs",
+  ylab="Frequency",
+  xlab="First digit")
